48f9c8cd7760eb8651244049189ec0613e7eda78
Mathilde Robert <m.robert@skema.edu>
2020-08-11 15:34:41 +0200

    Config + HP agenceur

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,33 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap';
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { scriptAgenceur } from "./plugins/script";
+
+// owlCarousel();
+// appear();
+// lazyLoad();
+// jqueryScrollbar();
+// TweenMaxJS();
+// swiper();
+// gmaps();
+// mapScript();
+// jQueryUserInterface();
+// jQueryBootstrapTouch();
+// jqueryCircleType();
+// jQueryLettering();
+// jQueryKnob();
+// languageJS();
+// mixItUp();
+// pageNav();
+// respondJS();
+// validateJS();
+scriptAgenceur();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +37,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// import 'css/site.scss'
