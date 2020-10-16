e527a1dccec38058902344c73aed8ea468ff9ba8
Mathilde Robert <m.robert@skema.edu>
2020-10-16 18:37:21 +0200

    modifs thomas

--- app/javascript/custom/popover.js
+++ app/javascript/custom/popover.js
@@ -1,7 +1,18 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+if(/Android|iPhone|iPod|BlackBerry|Opera Mini/i.test(navigator.userAgent)) {
+ $(function () {
+   $('[data-toggle="popover"]').popover({trigger: 'focus'});
+ })
 
-$('.popover-dismiss').popover({
-  trigger: 'focus'
-})
+ $('.popover-dismiss').popover({
+   trigger: 'focus'
+ })
+} else {
+  $(function () {
+    $('[data-toggle="popover"]').popover({trigger: 'hover'});
+  })
+
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  })
+}
+
