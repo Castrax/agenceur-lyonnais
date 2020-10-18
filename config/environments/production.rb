a38479224da1cea680fe691a798b6d1a7029a172
Mathilde Robert <m.robert@skema.edu>
2020-10-18 19:50:24 +0200

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,13 +114,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => 'apikey',
+  :password => ENV['SENDGRID_APIKEY'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
   }
-
 end
